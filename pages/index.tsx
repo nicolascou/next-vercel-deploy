72bda097f1be464816406baa6837656d1088efe4
nicolascou <nicolascousillas1@gmail.com>
2022-06-03 12:03:36 +0200

    First commit

---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react'
+import Link from 'next/link'
+import { MainLayout } from '../components/layouts/MainLayout'
+
+
+export default function HomePage () {
+
+  return (
+    <MainLayout>
+        <div>
+        <h1>Home Page</h1>
+        
+        <h1 className="title">
+          {/* Ir a <a href="/about">About</a> */}
+          Ir a <Link href="/about">About</Link>
+        </h1>
+
+        <p className={'description'}>
+          Get started by editing{' '}
+          <code className={'code'}>pages/index.js</code>
+        </p>
+        </div>
+    </MainLayout>
+  )
+}
